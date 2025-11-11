a26965b0e5deada95406f9557610b2a69cec785b
anzame <natadecocoex1026@gmail.com>
2025-11-11 21:46:25 +0900

    first commit

---
+++ naoino_blog/src/test/java/naoino/naoino_blog/NaoinoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package naoino.naoino_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaoinoBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
